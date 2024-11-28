4d3e32aa5e6005799f50e93a970e5c256c9477a0
Cawitos <155829173+Cawitos@users.noreply.github.com>
2024-11-28 17:10:47 -0500

    add/changes
    Se añade las 3 razas "gatos" "perros" "aves". Se cambia "especies.html" para ser completada

---
+++ mockups-animalbeats/js/razas.js
@@ -0,0 +1,16 @@
+function openModal(race) {
+    document.getElementById(race).style.display = "block";
+}
+
+function closeModal(race) {
+    document.getElementById(race).style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modals = document.getElementsByClassName('modal');
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
