ef68dad1a5459dd24a879471081e4cb98f20decf
cxm1lo <edwardlozanop@gmail.com>
2025-04-11 07:04:08 -0500

    change
    colores y links para el menu desplegable

--- mockups-animalbeats/Maquetacion-Logica/config.py
+++ mockups-animalbeats/Maquetacion-Logica/config.py
@@ -3,5 +3,5 @@ class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_DB = 'AnimalBeats'
-    MYSQL_PASSWORD = 'Holaoreo<3'
+    MYSQL_PASSWORD = ''
     SESSION_COOKIE_SECURE: False
