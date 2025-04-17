d620e86546f4ec954e5fa1c17b4dd8860888d136
Alej4ndr0-create <diegoalejandrovalbuenamancera@gmail.com>
2025-04-17 18:33:33 -0500

    Fix
    Base de datos arreglada
    Validacion de usuarios arreglada

--- mockups-animalbeats/script-AnimalBeats.sql
+++ mockups-animalbeats/script-AnimalBeats.sql
@@ -11,34 +11,16 @@ CREATE TABLE Documento(
 	tipo ENUM('T.I', 'C.C', 'C.E')
 );
 CREATE TABLE Usuarios(
-	n_documento VARCHAR(10) primary key,
-	correoelectronico VARCHAR(255)NOT NULL,
-	contrasena VARCHAR(255)NOT NULL,
+	n_documento VARCHAR(10) PRIMARY KEY,
+	correoelectronico VARCHAR(255) NOT NULL,
+	contrasena VARCHAR(255) NOT NULL,
 	id_documento INT,
-    estado enum('ACTIVO', 'INACTIVO'),
-	FOREIGN KEY (id_documento) REFERENCES Documento(id) on delete cascade
+	id_rol INT,
+	estado ENUM('ACTIVO', 'INACTIVO'),
+	FOREIGN KEY (id_documento) REFERENCES Documento(id) ON DELETE CASCADE,
+	FOREIGN KEY (id_rol) REFERENCES Rol(id)
 );
-CREATE TABLE Administrador(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_Usuario varchar(10) not null,
-    id_rol int not null,
-    FOREIGN KEY (id_rol) REFERENCES Rol(id) on delete cascade,
-    foreign key (id_Usuario) references Usuarios(n_documento) on delete cascade
-);
-CREATE TABLE Veterinario(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_Usuario varchar(10) not null,
-    id_rol int not null,
-    FOREIGN KEY (id_rol) REFERENCES Rol(id) on delete cascade,
-    foreign key (id_Usuario) references Usuarios(n_documento) on delete cascade
-);
-CREATE TABLE Cliente(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_Usuario varchar(10) not null,
-    id_rol int not null,
-    FOREIGN KEY (id_rol) REFERENCES Rol(id) on delete cascade,
-    foreign key (id_Usuario) references Usuarios(n_documento) on delete cascade
-);
+
 CREATE TABLE Especie (
     id INT AUTO_INCREMENT PRIMARY KEY,
     Especie VARCHAR(50)
@@ -53,14 +35,12 @@ CREATE TABLE Raza (
 );
 CREATE TABLE Mascota(
 	id int auto_increment primary key,
-    id_Cliente int not null,
     Nombre varchar(45) not null,
     id_Especie int not null,
     id_Raza int not null,
     estado enum('ACTIVO', 'INACTIVO'),
     edad int not null,
     foreign key (id_Raza) references Raza(id) on delete cascade,
-    foreign key (id_Cliente) references Cliente(id) on delete cascade,
     foreign key (id_Especie) references Especie(id) on delete cascade
 );
 create table Enfermedad(
@@ -72,27 +52,19 @@ Create table Servicios(
     servicio enum ('1','2','3','4','5')
 );
 Create table Citas(
-	id_Cliente int not null,
-    id_Veterinario int not null,
     id_Mascota int not null,
 	id_Servicio int not null,
     Fecha date not null,
     Descripcion varchar(255),
-    foreign key (id_Veterinario) references Veterinario(id) on delete cascade,
-    foreign key (id_Cliente) references Cliente(id) on delete cascade,
     foreign key (id_Mascota) references Mascota(id) on delete cascade,
     foreign key (id_Servicio) references Servicios(id) on delete cascade
 );
 Create table Alertas(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    id_Veterinario int not null, 
-    id_Cliente int not null,
     id_Mascota int not null,
     Fecha date not null,
     descripcion TEXT not null,
-    foreign key (id_Veterinario) references Veterinario(id) on delete cascade,
-	foreign key (id_Mascota) references Mascota(id) on delete cascade,
-    foreign key (id_Cliente) references Cliente(id) on delete cascade
+	foreign key (id_Mascota) references Mascota(id) on delete cascade
 );	
 
 INSERT INTO Documento (tipo) VALUES ('C.C'), ('T.I'), ('C.E');
