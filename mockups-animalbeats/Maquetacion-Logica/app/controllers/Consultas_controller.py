c67aa1831e1662c46b29235248328db26f63b0cc
cxm1lo <edwardlozanop@gmail.com>
2025-04-22 20:40:45 -0500

    fix

--- mockups-animalbeats/Maquetacion-Logica/app/controllers/Consultas_controller.py
+++ mockups-animalbeats/Maquetacion-Logica/app/controllers/Consultas_controller.py
@@ -4,4 +4,4 @@ Consulta_bp = Blueprint('Consulta_bp', __name__)
 
 @Consulta_bp.route('/consultas')
 def Consultas():
-    return render_template('Consultas.html')
+    return render_template('Administrador/Consultas.html')
