11e17cc9651502bc279697072e6a9b74e9133556
Cawitos <155829173+Cawitos@users.noreply.github.com>
2025-11-26 15:05:07 -0500

    update

--- script-AnimalBeats.sql
+++ script-AnimalBeats.sql
@@ -96,7 +96,7 @@ CREATE TABLE Recordatorios(
 -- Datos iniciales
 -- ===========================
 INSERT INTO Documento (tipo) VALUES ('C.C'), ('T.I'), ('C.E');
-INSERT INTO Rol (rol) VALUES ('admin'), ('cliente'), ('veterinario');
+INSERT INTO Rol (rol) VALUES ('admin'), ('tutor'), ('veterinario');
 INSERT INTO Servicios (servicio) VALUES 
     ('Consulta General'), 
     ('Urgencias'), 
